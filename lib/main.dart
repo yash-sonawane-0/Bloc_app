273bd2b25e46d35af5b5fe06f825c35fdbe809e3
yash-sonawane-4930 <yash@healthx.dev>
2022-10-25 15:53:58 +0530

    InitialCode

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:bloc_app/pages/weather_search_page.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      title: 'Material App',
+      home: WeatherSearchPage(),
+    );
+  }
+}
---
+++ lib/pages/weather_search_page.dart
@@ -0,0 +1,86 @@
+import 'package:bloc_app/data/model/weather.dart';
+import 'package:flutter/material.dart';
+
+class WeatherSearchPage extends StatefulWidget {
+  const WeatherSearchPage({super.key});
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _WeatherSearchPageState createState() => _WeatherSearchPageState();
+}
+
+class _WeatherSearchPageState extends State<WeatherSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Weather Search"),
+      ),
+      body: Container(
+        padding: const EdgeInsets.symmetric(vertical: 16),
+        alignment: Alignment.center,
+        // ignore: todo
+        // TODO: Implement with cubit
+        child: buildInitialInput(),
+      ),
+    );
+  }
+
+  Widget buildInitialInput() {
+    return const Center(
+      child: CityInputField(),
+    );
+  }
+
+  Widget buildLoading() {
+    return const Center(
+      child: CircularProgressIndicator(),
+    );
+  }
+
+  Column buildColumnWithData(Weather weather) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        Text(
+          weather.cityName,
+          style: const TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.w700,
+          ),
+        ),
+        Text(
+          // Display the temperature with 1 decimal place
+          "${weather.temperatureCelsius.toStringAsFixed(1)} °C",
+          style: const TextStyle(fontSize: 80),
+        ),
+        const CityInputField(),
+      ],
+    );
+  }
+}
+
+class CityInputField extends StatelessWidget {
+  const CityInputField({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      child: TextField(
+        onSubmitted: (value) => submitCityName(context, value),
+        textInputAction: TextInputAction.search,
+        decoration: InputDecoration(
+          hintText: "Enter a city",
+          border: OutlineInputBorder(borderRadius: BorderRadius.circular(12)),
+          suffixIcon: const Icon(Icons.search),
+        ),
+      ),
+    );
+  }
+
+  void submitCityName(BuildContext context, String cityName) {
+    // ignore: todo
+    // TODO: Get weather for the city
+  }
+}
